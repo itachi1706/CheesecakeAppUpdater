6ee93daea3f0b09d569626e691da9105eda09e96
itachi1706 <kennethsohyq@gmail.com>
2017-05-12 18:55:44 +0800

    Added Nougat Data Saver API into the connectivity helper class as per #8

--- src/main/java/com/itachi1706/appupdater/Util/ConnectivityHelper.java
+++ src/main/java/com/itachi1706/appupdater/Util/ConnectivityHelper.java
@@ -3,6 +3,8 @@ package com.itachi1706.appupdater.Util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 /**
  * Created by Kenneth on 3/3/2016.
@@ -18,9 +20,18 @@ public final class ConnectivityHelper {
      * @return Network Info
      */
     private static NetworkInfo getNetworkInfo(Context context) {
-        ConnectivityManager cm = (ConnectivityManager ) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager cm = getConnectivityManager(context);
 
         return cm.getActiveNetworkInfo();
+    }
+
+    /**
+     * Gets the Connectivity Manager object
+     * @param context Context
+     * @return Connectivity Manager
+     */
+    private static ConnectivityManager getConnectivityManager(Context context) {
+        return (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
     }
 
     /**
@@ -60,5 +71,40 @@ public final class ConnectivityHelper {
      */
     public static int getActiveNetworkType(Context context) {
         return getNetworkInfo(context).getType();
+    }
+
+    /**
+     * Whether the app should throttle usage of data
+     * This is really for devices running API 24 (Nougat) where there is the Data Saver option
+     * @param context Context
+     * @return true if it should be throttled, false otherwise
+     */
+    public static boolean shouldThrottle(Context context) {
+        ConnectivityManager manager = getConnectivityManager(context);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (manager.isActiveNetworkMetered()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    switch (manager.getRestrictBackgroundStatus()) {
+                        case ConnectivityManager.RESTRICT_BACKGROUND_STATUS_ENABLED: return true;
+                        case ConnectivityManager.RESTRICT_BACKGROUND_STATUS_WHITELISTED:
+                        case ConnectivityManager.RESTRICT_BACKGROUND_STATUS_DISABLED:
+                        default: return false;
+                    }
+                } else return false;
+            } else return false;
+        }
+        return false;
+    }
+
+    /**
+     * Gets the Data Saver Option for the application itself
+     * @param context Context
+     * @return int One of 3 choices
+     * {RESTRICT_BACKGROUND_STATUS_ENABLED, RESTRICT_BACKGROUND_STATUS_WHITELISTED, RESTRICT_BACKGROUND_STATUS_DISABLED}
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static int getDataSaverSetting(Context context) {
+        ConnectivityManager manager = getConnectivityManager(context);
+        return manager.getRestrictBackgroundStatus();
     }
 }
