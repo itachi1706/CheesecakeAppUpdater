7a6642528621688970cabb4b66668af887ad14f0
itachi1706 <kennethsohyq@gmail.com>
2017-11-25 23:10:27 +0800

    Updated dependencies and added fingerprint library into this lib

---
+++ src/main/java/com/itachi1706/appupdater/extlib/fingerprint/FingerprintLibBase.java
@@ -0,0 +1,97 @@
+package com.itachi1706.appupdater.extlib.fingerprint;
+
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.view.inputmethod.InputMethodManager;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+
+class FingerprintLibBase {
+
+    protected FingerprintLibBase(@NonNull Activity context, @NonNull String keyName, @NonNull FingerprintLibCallback callback) {
+        mContext = context;
+        mKeyName = keyName;
+        mCallback = callback;
+
+        mInputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            MUtils.initBase(context, this);
+    }
+
+    protected void deinitBase() {
+        mKeyName = null;
+        mContext = null;
+        mKeyguardManager = null;
+        mFingerprintManager = null;
+        mKeyStore = null;
+        mKeyGenerator = null;
+        mCipher = null;
+    }
+
+    protected String mKeyName;
+    protected Context mContext;
+    protected KeyguardManager mKeyguardManager;
+    protected FingerprintManager mFingerprintManager;
+    protected InputMethodManager mInputMethodManager;
+    protected KeyStore mKeyStore;
+    protected KeyGenerator mKeyGenerator;
+    protected Cipher mCipher;
+    protected FingerprintLibCallback mCallback;
+
+    public void setCallback(@NonNull FingerprintLibCallback callback) {
+        this.mCallback = callback;
+    }
+
+    /**
+     * Initialize the {@link Cipher} instance with the created key in the {@link #recreateKey()}
+     * method.
+     *
+     * @return {@code true} if initialization is successful, {@code false} if the lock screen has
+     * been disabled or reset after the key was generated, or if a fingerprint got enrolled after
+     * the key was generated.
+     */
+    protected boolean initCipher() {
+        return MUtils.initCipher(this);
+    }
+
+    /**
+     * Creates a symmetric key in the Android Key Store which can only be used after the user has
+     * authenticated with fingerprint.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public final void recreateKey() {
+        // The enrolling flow for fingerprint. This is where you ask the user to set up fingerprint
+        // for your flow. Use of keys is necessary if you need to know if the set of
+        // enrolled fingerprints has changed.
+        try {
+            mKeyStore.load(null);
+            // Set the alias of the entry in Android KeyStore where the key will appear
+            // and the constrains (purposes) in the constructor of the Builder
+            mKeyGenerator.init(new KeyGenParameterSpec.Builder(mKeyName,
+                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    // Require the user to authenticate with a fingerprint to authorize every use
+                    // of the key
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            mKeyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | CertificateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/itachi1706/appupdater/extlib/fingerprint/MUtils.java
@@ -0,0 +1,83 @@
+package com.itachi1706.appupdater.extlib.fingerprint;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
+import android.support.v4.content.ContextCompat;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+@TargetApi(Build.VERSION_CODES.M)
+class MUtils {
+
+    private MUtils() {
+    }
+
+    public static boolean isFingerprintRegistered(FingerprintLib fingerprintLib) {
+        if (!isFingerprintAuthAvailable(fingerprintLib)) return false;
+        //noinspection ResourceType
+        return fingerprintLib.mKeyguardManager.isKeyguardSecure() && fingerprintLib.mFingerprintManager.hasEnrolledFingerprints();
+    }
+
+    public static boolean isFingerprintAuthAvailable(FingerprintLib fingerprintLib) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return false;
+        int granted = ContextCompat.checkSelfPermission(fingerprintLib.mContext, Manifest.permission.USE_FINGERPRINT);
+        if (granted != PackageManager.PERMISSION_GRANTED) return false;
+        //noinspection ResourceType
+        return fingerprintLib.mFingerprintManager.isHardwareDetected() && fingerprintLib.mFingerprintManager.hasEnrolledFingerprints();
+    }
+
+    public static void initBase(Context context, FingerprintLibBase fingerprintLib) {
+        fingerprintLib.mKeyguardManager = context.getSystemService(KeyguardManager.class);
+        fingerprintLib.mFingerprintManager = context.getSystemService(FingerprintManager.class);
+        try {
+            fingerprintLib.mKeyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (KeyStoreException e) {
+            throw new RuntimeException("Failed to get an instance of KeyStore", e);
+        }
+        try {
+            fingerprintLib.mKeyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException("Failed to get an instance of KeyGenerator", e);
+        }
+        try {
+            fingerprintLib.mCipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                    + KeyProperties.BLOCK_MODE_CBC + "/"
+                    + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get an instance of Cipher", e);
+        }
+    }
+
+    public static boolean initCipher(FingerprintLibBase fingerprintLib) {
+        try {
+            fingerprintLib.mKeyStore.load(null);
+            SecretKey key = (SecretKey) fingerprintLib.mKeyStore.getKey(fingerprintLib.mKeyName, null);
+            fingerprintLib.mCipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException
+                | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+}
