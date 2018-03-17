a0e25a1f036fcffd6193e9e411869bff209973b7
itachi1706 <kennethsohyq@gmail.com>
2018-03-17 23:49:31 +0800

    Added a analytics data provider
    This provider will generate data based on whether the checkbox is enabled or not. It is up to the developer to decide on whether to make it a opt-in or opt-out option through the user of the "defaultMode" boolean in the class constructor
    
    Closes #20

---
+++ src/main/java/com/itachi1706/appupdater/Util/AnalyticsHelper.java
@@ -0,0 +1,97 @@
+package com.itachi1706.appupdater.Util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
+
+import com.itachi1706.appupdater.BuildConfig;
+import com.itachi1706.appupdater.Objects.CAAnalytics;
+
+/**
+ * Created by Kenneth on 17/3/2018.
+ * for com.itachi1706.appupdater.Util in CheesecakeUtilities
+ */
+
+public class AnalyticsHelper {
+
+    private SharedPreferences mSharedPreference;
+    private boolean mDefaultMode;
+    @Nullable private PackageInfo pInfo;
+    private static final String ANALYTICS_PREF = "ca_analytics";
+
+    /**
+     * Creates the Analytics Helper object
+     * @param context Application Context
+     * @param defaultMode Whether it should be enabled by default or not
+     */
+    public AnalyticsHelper(Context context, boolean defaultMode) {
+        this.mSharedPreference = PreferenceManager.getDefaultSharedPreferences(context);
+        this.mDefaultMode = defaultMode;
+        try {
+            this.pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            this.pInfo = null;
+        }
+    }
+
+    /**
+     * Check if analytics is enabled
+     * @return true if enabled, false otherwise
+     */
+    public boolean isEnabled() {
+        return mSharedPreference.contains(ANALYTICS_PREF) && mSharedPreference.getBoolean(ANALYTICS_PREF, mDefaultMode); // Manual opt out
+    }
+
+
+    /**
+     * Get relevant data if analytics is not disabled. You can choose which of the data to create as custom properties
+     * Remeber to follow https://firebase.google.com/docs/analytics/android/properties#set_user_properties for more information if using Firebase
+     * Sample user properties fields: debug_mode, device_manufacturer, device_model, device_codename, device_fingerprint, device_cpu_abi,
+     * device_tags, app_version_code, app_version, android_sdk_version, android_version, android_sec_patch
+     * @return Analytics data or null if not enabled
+     */
+    @Nullable
+    @WorkerThread
+    public CAAnalytics getData() {
+        if (!isEnabled()) return null;
+
+        //Debug Info Get
+        String version = "Unknown";
+        int versionCode = 0;
+        if (pInfo != null) {
+            version = pInfo.versionName;
+            versionCode = pInfo.versionCode;
+        }
+        boolean newAbiData = false;
+
+        // Generate data
+        CAAnalytics analytics = new CAAnalytics();
+        analytics.setDebug(BuildConfig.DEBUG);
+        analytics.setdManufacturer(Build.MANUFACTURER);
+        analytics.setdName(Build.MODEL);
+        analytics.setAppVerCode(versionCode);
+        analytics.setAppVer(version);
+        analytics.setSdkString(Build.VERSION.RELEASE);
+        analytics.setSdkver(Build.VERSION.SDK_INT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            String[] support = Build.SUPPORTED_ABIS;
+            if (support.length > 0) {
+                analytics.setdCPU(support[0]);
+                newAbiData = true;
+            }
+        }
+        if (!newAbiData) analytics.setdCPU(Build.CPU_ABI);
+        analytics.setdFingerprint(Build.FINGERPRINT);
+        analytics.setdTags(Build.TAGS);
+        analytics.setdCodename(Build.PRODUCT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            analytics.setSdkPatch(Build.VERSION.SECURITY_PATCH);
+        else analytics.setSdkPatch("1970-01-01");
+        return analytics;
+    }
+}
