48f629558c20c64c0bd255aafa2665f7640f7352
itachi1706 <kennethsohyq@gmail.com>
2018-07-17 16:55:06 +0800

    Move signature generation to util class to use across all apps

--- src/main/java/com/itachi1706/appupdater/Util/ValidationHelper.java
+++ src/main/java/com/itachi1706/appupdater/Util/ValidationHelper.java
@@ -1,7 +1,18 @@
 package com.itachi1706.appupdater.Util;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
+import java.io.ByteArrayInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -52,6 +63,67 @@ public final class ValidationHelper {
         if (playstoreList.contains(installer)) return GOOGLE_PLAY;
         if (amazonList.contains(installer)) return AMAZON;
         return SIDELOAD;
+    }
+
+    // Signature Validation
+    @Nullable
+    public static String getSignatureForValidation(Context context) {
+        PackageManager pm = context.getPackageManager();
+        Signature[] signatures = null;
+        try {
+            PackageInfo pInfo;
+            try {
+                pInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            } catch (RuntimeException e) {
+                Log.e("ValidationHelper", "Failed to get package info. Signature cannot be validated");
+                return null;
+            }
+            signatures = pInfo.signatures;
+            return getSignatureString(signatures[0]).trim();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            Log.e("ValidationHelper", "Failed to get package info. Signature cannot be validated");
+            return null;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            Log.e("ValidationHelper", "Algorithm not recognized on this Android Version, signature cannot be validated");
+            return null;
+        }
+    }
+
+    public static X509Certificate getCert(byte[] cert) throws CertificateException {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(cert);
+        CertificateFactory cf = CertificateFactory.getInstance("X509");
+        return (X509Certificate) cf.generateCertificate(inputStream);
+    }
+
+    public static String getSignatureString(Signature sig) throws NoSuchAlgorithmException {
+        byte[] cert = sig.toByteArray();
+        try {
+            return bytesToHex(MessageDigest.getInstance("SHA1").digest(getCert(cert).getEncoded()));
+        } catch (CertificateException e) {
+            Log.e("Signature", "Cannot Create Signature, Falling back");
+            Log.e("Signature", "Error: " + e.getLocalizedMessage());
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            md.update(sig.toByteArray());
+            return bytesToHex(md.digest());
+        }
+    }
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hexChars.length; i++) {
+            if (i % 2 == 0 && i != 0) sb.append(":");
+            sb.append(hexChars[i]);
+        }
+        return sb.toString();
     }
 
 
