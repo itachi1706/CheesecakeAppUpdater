52c9633694411115b7bbd08eaa2a13461739d83e
itachi1706 <kennethsohyq@gmail.com>
2019-01-27 17:51:45 +0800

    Added drag and drop to the logs itself

--- src/main/java/com/itachi1706/appupdater/ViewLogsActivity.java
+++ src/main/java/com/itachi1706/appupdater/ViewLogsActivity.java
@@ -1,10 +1,15 @@
 package com.itachi1706.appupdater;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -12,23 +17,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewLogsActivity extends AppCompatActivity {
 
     private TextView logText;
+    private int height, width;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ScrollView v = new ScrollView(this);
-        setContentView(v);
         logText = new TextView(this);
-        v.addView(logText);
+        setContentView(v);
 
-        //logText.setMovementMethod(new ScrollingMovementMethod());
-        //logText.setVerticalScrollBarEnabled(true);
+        v.addView(logText);
+        logText.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) return longClickBtn(v);
+                return true;
+            }
+        });
 
         StringBuilder log = new StringBuilder();
         try {
@@ -44,6 +56,11 @@ public class ViewLogsActivity extends AppCompatActivity {
             Log.e("ViewLogs", "Error viewing app log. Exception: " + e.toString());
             e.printStackTrace();
         }
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        height = displayMetrics.heightPixels - 100;
+        width = displayMetrics.widthPixels - 100;
     }
 
     @Override
@@ -62,5 +79,33 @@ public class ViewLogsActivity extends AppCompatActivity {
             startActivity(Intent.createChooser(shareTextIntent, "Share Logs for " + getPackageName() + " with"));
             return true;
         } else return super.onOptionsItemSelected(item);
+    }
+
+    @RequiresApi(api = android.os.Build.VERSION_CODES.N)
+    public boolean longClickBtn(View v) {
+        ClipData clip = ClipData.newPlainText("logs", ((TextView) v).getText().toString());
+        View.DragShadowBuilder dragShadowBuilder = new LogsDragShadowBuilder(v);
+        v.startDragAndDrop(clip, dragShadowBuilder, true, View.DRAG_FLAG_GLOBAL|View.DRAG_FLAG_GLOBAL_URI_READ|
+                View.DRAG_FLAG_GLOBAL_PERSISTABLE_URI_PERMISSION);
+        return true;
+    }
+
+    class LogsDragShadowBuilder extends View.DragShadowBuilder {
+
+        LogsDragShadowBuilder(View view) {
+            super(view);
+        }
+
+        @Override
+        public void onProvideShadowMetrics(Point outShadowSize, Point outShadowTouchPoint) {
+            final View view = super.getView();
+            if (view != null && view.getWidth() > 0 && view.getHeight() > 0) {
+                outShadowSize.set((view.getWidth() > width) ? width : view.getWidth(), (view.getHeight() > height) ? height : view.getHeight());
+                outShadowTouchPoint.set((outShadowSize.x > width) ? width / 2 : outShadowSize.x / 2, (outShadowSize.y > height) ? height / 2 : outShadowSize.y / 2);
+            } else {
+                outShadowSize.set(1,1);
+                outShadowTouchPoint.set(0,0);
+            }
+        }
     }
 }
