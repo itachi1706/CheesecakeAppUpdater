7e0f2d8fa5300c1356f8e986b9abca09a98aab69
itachi1706 <kennethsohyq@gmail.com>
2019-05-15 22:20:01 +0800

    Made proguard ignore the updater

--- src/main/java/com/itachi1706/appupdater/Objects/AppUpdateMessageObject.java
+++ src/main/java/com/itachi1706/appupdater/Objects/AppUpdateMessageObject.java
@@ -1,10 +1,13 @@
 package com.itachi1706.appupdater.Objects;
 
+import androidx.annotation.Keep;
+
 /**
  * Created by itachi1706 on 2/20/2016.
  * For com.itachi1706.appupdate.Objects in AppUpdater
  */
 @SuppressWarnings("unused")
+@Keep
 public final class AppUpdateMessageObject {
     private int index;
     private String id, appid, updateText, dateModified, versionCode, versionName, labels, url;
--- src/main/java/com/itachi1706/appupdater/Objects/AppUpdateObject.java
+++ src/main/java/com/itachi1706/appupdater/Objects/AppUpdateObject.java
@@ -1,10 +1,13 @@
 package com.itachi1706.appupdater.Objects;
 
+import androidx.annotation.Keep;
+
 /**
  * Created by itachi1706 on 2/20/2016.
  * For com.itachi1706.appupdate.Objects in AppUpdater
  */
 @SuppressWarnings("unused")
+@Keep
 public final class AppUpdateObject {
     private int index;
     private String id, packageName, appName, dateCreated, latestVersion, latestVersionCode, apptype;
