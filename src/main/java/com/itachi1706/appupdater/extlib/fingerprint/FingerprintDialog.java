ad08c92be81a7111490dfca791d904900a8bc2de
itachi1706 <kennethsohyq@gmail.com>
2019-06-07 20:33:54 +0800

    Deprecated FP reader libs
    This is because it uses Material Dialog which means when I have to upgrade it will take more dev hours. On top of that there is already a BiometricCompat library to handle this as well so its not needed

--- src/main/java/com/itachi1706/appupdater/extlib/fingerprint/FingerprintDialog.java
+++ src/main/java/com/itachi1706/appupdater/extlib/fingerprint/FingerprintDialog.java
@@ -5,13 +5,6 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.core.content.ContextCompat;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -19,6 +12,13 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -30,6 +30,7 @@ import com.itachi1706.appupdater.R;
  * authentication if fingerprint is not available.
  */
 @SuppressWarnings("ResourceType")
+@Deprecated
 public class FingerprintDialog extends DialogFragment
         implements TextView.OnEditorActionListener, FingerprintLibCallback {
 
