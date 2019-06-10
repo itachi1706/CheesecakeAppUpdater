c5fd2f892a1850ef87aa5967c1c3aaeb4eebeb05
itachi1706 <kennethsohyq@gmail.com>
2019-06-10 16:38:25 +0800

    Added ability to handle night mode changes

--- src/main/java/com/itachi1706/appupdater/Util/PrefHelper.java
+++ src/main/java/com/itachi1706/appupdater/Util/PrefHelper.java
@@ -2,8 +2,12 @@ package com.itachi1706.appupdater.Util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatDelegate;
 
 /**
  * Created by Kenneth on 19/12/2018.
@@ -24,5 +28,30 @@ public class PrefHelper {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
         StrictMode.setThreadPolicy(old);
         return sp;
+    }
+
+    /**
+     * Check if night mode is enabled
+     * @param context Activity Context
+     * @return false if Night mode is disabled, true otherwise
+     */
+    public static boolean isNightModeEnabled(Context context) {
+        int currentNightMode = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        return currentNightMode != Configuration.UI_MODE_NIGHT_NO;
+    }
+
+    /**
+     * Set Night Mode Theme. Options include:
+     * {@link AppCompatDelegate#MODE_NIGHT_NO}
+     * {@link AppCompatDelegate#MODE_NIGHT_YES}
+     * {@link AppCompatDelegate#MODE_NIGHT_FOLLOW_SYSTEM}
+     * {@link AppCompatDelegate#MODE_NIGHT_AUTO}
+     * {@link AppCompatDelegate#MODE_NIGHT_AUTO_BATTERY}
+     * @param newTheme New Theme setting
+     * @param themeName THeme name for logging purposes
+     */
+    public static void changeDarkModeTheme(int newTheme, String themeName) {
+        Log.i("AppThemeChanger", "Switching over to " + themeName + " mode");
+        AppCompatDelegate.setDefaultNightMode(newTheme);
     }
 }
