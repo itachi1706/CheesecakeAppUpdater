5a6b359d72fd696f00880985b06000626f98a287
itachi1706 <kennethsohyq@gmail.com>
2019-07-24 21:10:27 +0800

    Removed retrieval of non resettable device IDs like serial
    Removed READ_PHONE_STATE permission as it is no longer needed
    Closes #35

--- src/main/java/com/itachi1706/appupdater/DebugInfoActivity.java
+++ src/main/java/com/itachi1706/appupdater/DebugInfoActivity.java
@@ -1,19 +1,11 @@
 package com.itachi1706.appupdater;
 
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
 
 
 public class DebugInfoActivity extends AppCompatActivity {
@@ -56,57 +48,10 @@ public class DebugInfoActivity extends AppCompatActivity {
             findPreference("view_model_ver").setSummary(Build.MODEL);
             findPreference("view_product_ver").setSummary(Build.PRODUCT);
             findPreference("view_radio_ver").setSummary((Build.getRadioVersion().isEmpty()) ? "Unavailable" : Build.getRadioVersion());
-            findPreference("view_serial_ver").setSummary(requestSerial());
             findPreference("view_tags_ver").setSummary(Build.TAGS);
             findPreference("view_type_ver").setSummary(Build.TYPE);
             if (Build.USER != null) {
                 findPreference("view_user_ver").setSummary(Build.USER);
-            }
-        }
-
-        private static final int RC_PHONE_STATE = 3;
-
-        private String requestSerial() {
-            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                findPreference("view_serial_ver").setSelectable(true);
-                findPreference("view_serial_ver").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                    @RequiresApi(api = Build.VERSION_CODES.M)
-                    @Override
-                    public boolean onPreferenceClick(Preference preference) {
-                        Log.i("PermMan", "Requesting READ_PHONE_STATE permission to view serial number");
-                        requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, RC_PHONE_STATE);
-                        return false;
-                    }
-                });
-                return "Requires READ_PHONE_STATE permission to view. Click to grant permission";
-            }
-            return getSerial();
-        }
-
-        @SuppressLint({"HardwareIds", "MissingPermission"})
-        private String getSerial() {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                return Build.getSerial();
-            } else {
-                //noinspection deprecation
-                return Build.SERIAL;
-            }
-        }
-
-        @Override
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-            switch (requestCode) {
-                case RC_PHONE_STATE:
-                    if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        Log.d("PermMan", "Phone State Permission granted, Updating SERIAL");
-                        findPreference("view_serial_ver").setSummary(getSerial());
-                        return;
-                    }
-                    Log.d("PermMan", "Phone State Permission not granted, Displaying rationale for permission");
-                    Toast.makeText(getActivity(), "READ_PHONE_STATE permission not granted, SERIAL cannot be retrieved", Toast.LENGTH_LONG).show();
-                    break;
-                    default:
-                        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             }
         }
     }
