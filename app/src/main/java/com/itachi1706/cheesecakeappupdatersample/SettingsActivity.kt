1da99a8449257c35e6e3aadaeab6f9b00b2db5cd
itachi1706 <kennethsohyq@gmail.com>
2019-12-29 13:28:23 +0800

    CAUANDLIB-44 Added a sample app for the library for testing purposes
    This sample consist of an Android application that will be used to test
    the library involved
    
    Resolves CAUANDLIB-44

---
+++ app/src/main/java/com/itachi1706/cheesecakeappupdatersample/SettingsActivity.kt
@@ -0,0 +1,46 @@
+package com.itachi1706.cheesecakeappupdatersample
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.itachi1706.appupdater.EasterEggResMultiMusicPrefFragment
+import com.itachi1706.appupdater.SettingsInitializer
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager.beginTransaction().replace(R.id.settings, SettingsFragment()).commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == android.R.id.home) { finish(); true } else super.onOptionsItemSelected(item)
+    }
+
+    class SettingsFragment : EasterEggResMultiMusicPrefFragment() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            SettingsInitializer().setFullscreen(true).explodeUpdaterSettings(activity, R.mipmap.ic_launcher, "https://api.itachi1706.com/api/appupdatechecker.php?action=androidretrievedata&packagename=",
+                resources.getString(R.string.link_legacy), resources.getString(R.string.link_updates), this)
+            super.addEggMethods(false, null, true) { Toast.makeText(context, "This will launch about app", Toast.LENGTH_SHORT).show(); true }
+        }
+
+        override fun getMusicResource(): Int {
+            return R.raw.sample_lq
+        }
+
+        override fun getStopEggButtonText(): String {
+            return "Stop"
+        }
+
+        override fun getEndEggMessage(): String {
+            return "Music Stopped"
+        }
+
+        override fun getStartEggMessage(): String {
+            return "Music Start"
+        }
+    }
+}
