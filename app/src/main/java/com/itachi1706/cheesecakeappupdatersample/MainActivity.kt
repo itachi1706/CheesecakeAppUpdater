60548b57911feb22fd37c04bb782f95f724ed461
itachi1706 <kennethsohyq@gmail.com>
2019-12-29 12:49:46 +0800

    CAUANDLIB-44 Created a sample application
    This application will be a sample application that will be used for testing the android library

---
+++ app/src/main/java/com/itachi1706/cheesecakeappupdatersample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.itachi1706.cheesecakeappupdatersample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
