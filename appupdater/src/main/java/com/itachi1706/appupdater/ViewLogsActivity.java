4571572ad1caafe30d3ad3e5e1d5b7249da54e69
itachi1706 <kennethsohyq@gmail.com>
2020-07-01 21:16:24 +0800

    Fixed crash sharing application logs if it is too large
    Now instead of always attempting to share the plaintext directly, if it is too large, it will save it into a text file to be shared instead
    
    We also added a prompt if long press to drag and drop fails due to the same reason

--- appupdater/src/main/java/com/itachi1706/appupdater/ViewLogsActivity.java
+++ appupdater/src/main/java/com/itachi1706/appupdater/ViewLogsActivity.java
@@ -2,9 +2,13 @@ package com.itachi1706.appupdater;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.TransactionTooLargeException;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
@@ -13,12 +17,18 @@ import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.itachi1706.helperlib.utils.NotifyUserUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class ViewLogsActivity extends AppCompatActivity {
 
@@ -69,21 +79,64 @@ public class ViewLogsActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.ccn_share_logcat) {
+            final String SHARE_TAG = "ShareLogs";
+            Log.i(SHARE_TAG, "Attempting to share Application log via plaintext"); // Try sharing plaintext of logs first
             Intent shareTextIntent = new Intent(Intent.ACTION_SEND);
             shareTextIntent.setType("text/plain");
-            shareTextIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Application Logcat for " + getPackageName());
-            shareTextIntent.putExtra(Intent.EXTRA_TEXT, getPackageName() + " Logs:\n\n" + logText.getText().toString());
-            startActivity(Intent.createChooser(shareTextIntent, "Share Logs for " + getPackageName() + " with"));
+            shareTextIntent.putExtra(Intent.EXTRA_SUBJECT, "Application Logcat for " + getPackageName());
+            String logTextFull = getPackageName() + " Logs:\n\n" + logText.getText().toString();
+            shareTextIntent.putExtra(Intent.EXTRA_TEXT, logTextFull);
+            try {
+                startActivity(Intent.createChooser(shareTextIntent, "Share Logs for " + getPackageName() + " with"));
+            } catch (RuntimeException e) {
+                if (e.getCause() instanceof TransactionTooLargeException) {
+                    // Too large for intent, switching to save text file and sharing it
+                    Log.w(SHARE_TAG, "Application logs too large, converting to file for sharing");
+                    File tmp = new File(getCacheDir(), "logs" + File.separator + "log-" + System.currentTimeMillis() + ".txt");
+                    if (!saveLogToFile(tmp, logTextFull)) {
+                        NotifyUserUtil.createShortToast(getApplicationContext(), "Unable to share application logs");
+                        return true;
+                    }
+                    Log.i(SHARE_TAG, "Temp log file created. Generating uri for share");
+                    Intent shareFileIntent = new Intent(Intent.ACTION_SEND);
+                    shareFileIntent.setType("text/plain");
+                    Uri logUri = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) ? FileProvider.getUriForFile(getBaseContext(),
+                            getApplicationContext().getPackageName() + ".appupdater.provider", tmp) : Uri.fromFile(tmp);
+                    Log.d(SHARE_TAG, "Uri generated: " + logUri);
+                    shareFileIntent.putExtra(Intent.EXTRA_SUBJECT, "Application Logcat for " + getPackageName());
+                    shareFileIntent.putExtra(Intent.EXTRA_STREAM, logUri);
+                    Intent chooserIntent = Intent.createChooser(shareFileIntent, "Share Log file for " + getPackageName() + " with");
+                    List<ResolveInfo> resInfoList = this.getPackageManager().queryIntentActivities(chooserIntent, PackageManager.MATCH_DEFAULT_ONLY); // Grant permissions for intents
+                    for (ResolveInfo resolveInfo : resInfoList) this.grantUriPermission(resolveInfo.activityInfo.packageName, logUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(chooserIntent);
+                } else NotifyUserUtil.createShortToast(getApplicationContext(), "Error sharing application logs: " + e.getCause());
+            }
             return true;
         } else return super.onOptionsItemSelected(item);
+    }
+
+    private boolean saveLogToFile(File file, String logs) {
+        try {
+            if (file.exists() && !file.delete()) return false;
+            //noinspection ResultOfMethodCallIgnored
+            file.getParentFile().mkdirs();
+            FileWriter fw = new FileWriter(file);
+            fw.append(logs);
+            fw.flush();
+            fw.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @RequiresApi(api = android.os.Build.VERSION_CODES.N)
     public boolean longClickBtn(View v) {
         ClipData clip = ClipData.newPlainText("logs", ((TextView) v).getText().toString());
         View.DragShadowBuilder dragShadowBuilder = new LogsDragShadowBuilder(v);
-        v.startDragAndDrop(clip, dragShadowBuilder, true, View.DRAG_FLAG_GLOBAL|View.DRAG_FLAG_GLOBAL_URI_READ|
-                View.DRAG_FLAG_GLOBAL_PERSISTABLE_URI_PERMISSION);
+        if (!v.startDragAndDrop(clip, dragShadowBuilder, true, View.DRAG_FLAG_GLOBAL | View.DRAG_FLAG_GLOBAL_URI_READ | View.DRAG_FLAG_GLOBAL_PERSISTABLE_URI_PERMISSION))
+            NotifyUserUtil.createShortToast(getApplicationContext(), "Application logs are too large to do this. Please share it instead");
         return true;
     }
 
