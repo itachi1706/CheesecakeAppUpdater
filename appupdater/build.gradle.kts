1e954e89c6f6371bd5ab6134e47b91563399665a
Kenneth Soh <kenneth@itachi1706.com>
2026-04-23 17:40:37 +0800

    build: Cleaned up jacoco dependencies

--- appupdater/build.gradle.kts
+++ appupdater/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.jacoco)
 }
 
 ext.set("version", "4.1.1")
@@ -44,10 +43,6 @@ android {
             withJavadocJar()
         }
     }
-
-//    jacoco {
-//        version = "0.8.8"
-//    }
 }
 
 dependencies {
@@ -64,46 +59,5 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     api(libs.helperlib)
 }
-
-//tasks.register<JacocoReport>("jacocoTestReport") {
-//    dependsOn("testDebugUnitTest")
-//    group = "verification"
-//    description = "Generate Jacoco coverage reports for the debug build."
-//
-//    reports {
-//        xml.required.set(true)
-//        html.required.set(true)
-//    }
-//
-//    val fileFilter = listOf(
-//        "**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*",
-//        "**/*Test*.*", "android/**/*.*"
-//    )
-//
-//    val buildDir = layout.buildDirectory.get().asFile.path
-//
-//    val debugTree = fileTree("$buildDir/intermediates/javac/debug/classes") {
-//        exclude(fileFilter)
-//    }
-//    val kotlinDebugTree = fileTree("$buildDir/tmp/kotlin-classes/debug") {
-//        exclude(fileFilter)
-//    }
-//
-//    val mainSrc = "$projectDir/src/main/java"
-//    val kotlinSrc = "$projectDir/src/main/kotlin"
-//
-//    sourceDirectories.setFrom(files(mainSrc, kotlinSrc))
-//    classDirectories.setFrom(files(debugTree, kotlinDebugTree))
-//
-//    executionData.setFrom(
-//        fileTree(buildDir) {
-//            include(
-//                "jacoco/testDebugUnitTest.exec",
-//                "outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec"
-//            )
-//        }
-//    )
-//
-//}
 
 apply(from = "./publish.gradle")
